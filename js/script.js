6a0bc51610480b3bbe29bc0d9a963ba7bc0f16f3
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-11-04 12:16:27 +0530

    icons added in pages and modified navbar pointor events

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav_links');
+const nav_bar = document.getElementById('nav-bar');
 
 
 hamburger.addEventListener("click", () => {
@@ -12,7 +13,9 @@ function toggleNavbar() {
     const body = document.querySelector('html');
     if (body.style.overflow === "hidden") {
         body.style.overflow = "auto";
+        nav_bar.style.pointerEvents = "none";
     } else {
         body.style.overflow = "hidden";
+        nav_bar.style.pointerEvents = "all";
     }
 }
